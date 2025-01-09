a4135c1675b2f22174b578e9a2dcdcfa4b6c7b17
CuiCuiSha233 <65383983+CuiCuiSha233@users.noreply.github.com>
2025-01-10 00:05:26 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -90,7 +90,8 @@ export default defineNuxtConfig({
   // env
   runtimeConfig: {
     apiUrl: process.env.API_URL || "https://api.uptimerobot.com/v2/getMonitors?",
-    apiKey: process.env.API_KEY || "ur1884674-11dfb1e726368ce9a232256f",
+    //apiKey: process.env.API_KEY || "ur1884674-11dfb1e726368ce9a232256f",
+    apiKey: process.env.API_KEY,
     sitePassword: process.env.SITE_PASSWORD,
     siteSecretKey: process.env.SITE_SECRE_KEY || "site-status",
     public: siteConfig,
